18d22b7dad8d80a2bcb49ff88fa9a1f0e747b527
stil <lipvig@lipvig.net>
2015-10-31 21:09:29 +0100

    Add methods toArray(), parseString(), fromHsl() to Color class

--- src/Color.php
+++ src/Color.php
@@ -1,6 +1,10 @@
 <?php
 namespace GDText;
 
+/**
+ * 8-bit RGB color representation.
+ * @package GDText
+ */
 class Color
 {
     /**
@@ -38,6 +42,70 @@ class Color
     }
 
     /**
+     * Parses string to Color object representation.
+     * @param string $str String with color information, ex. #000000
+     * @return Color
+     * @todo Add parsing of CSS-like strings: rgb(), rgba(), hsl()
+     */
+    public static function parseString($str)
+    {
+        $str = str_replace('#', '', $str);
+        if (strlen($str) == 6) {
+            $r = hexdec(substr($str, 0, 2));
+            $g = hexdec(substr($str, 2, 2));
+            $b = hexdec(substr($str, 4, 2));
+        } else if (strlen($str) == 3) {
+            $r = hexdec(str_repeat(substr($str, 0, 1), 2));
+            $g = hexdec(str_repeat(substr($str, 1, 1), 2));
+            $b = hexdec(str_repeat(substr($str, 2, 1), 2));
+        } else {
+            throw new \InvalidArgumentException('Unrecognized color.');
+        }
+
+        return new Color($r, $g, $b);
+    }
+
+    /**
+     * @param float $h Hue
+     * @param float $s Saturation
+     * @param float $l Light
+     * @return Color
+     */
+    public static function fromHsl($h, $s, $l)
+    {
+        $fromFloat = function (array $rgb) {
+            foreach ($rgb as &$v) {
+                $v = (int)round($v * 255);
+            };
+
+            return new Color($rgb[0], $rgb[1], $rgb[2]);
+        };
+
+        // If saturation is 0, the given color is grey and only
+        // lightness is relevant.
+        if ($s == 0) {
+            return $fromFloat(array($l, $l, $l));
+        }
+
+        // Else calculate r, g, b according to hue.
+        // Check http://en.wikipedia.org/wiki/HSL_and_HSV#From_HSL for details
+        $chroma = (1 - abs(2 * $l - 1)) * $s;
+        $h_     = $h * 6;
+        $x      = $chroma * (1 - abs((fmod($h_,2)) - 1)); // Note: fmod because % (modulo) returns int value!!
+        $m = $l - round($chroma/2, 10); // Bugfix for strange float behaviour (e.g. $l=0.17 and $s=1)
+
+        if ($h_ >= 0 && $h_ < 1) $rgb = array(($chroma + $m), ($x + $m), $m);
+        elseif ($h_ >= 1 && $h_ < 2) $rgb = array(($x + $m), ($chroma + $m), $m);
+        elseif ($h_ >= 2 && $h_ < 3) $rgb = array($m, ($chroma + $m), ($x + $m));
+        elseif ($h_ >= 3 && $h_ < 4) $rgb = array($m, ($x + $m), ($chroma + $m));
+        elseif ($h_ >= 4 && $h_ < 5) $rgb = array(($x + $m), $m, ($chroma + $m));
+        elseif ($h_ >= 5 && $h_ < 6) $rgb = array(($chroma + $m), $m, ($x + $m));
+        else throw new \InvalidArgumentException('Invalid hue, it should be a value between 0 and 1.');
+
+        return $fromFloat($rgb);
+    }
+
+    /**
      * @param resource $image GD image resource
      * @return int Returns the index of the specified color+alpha in the palette of the image,
      *             or index of allocated color if the color does not exist in the image's palette.
@@ -67,5 +135,13 @@ class Color
     public function hasAlphaChannel()
     {
         return $this->alpha !== null;
+    }
+
+    /**
+     * @return int[]
+     */
+    public function toArray()
+    {
+        return array($this->red, $this->green, $this->blue);
     }
 }
--- tests/ColorTest.php
+++ tests/ColorTest.php
@@ -29,4 +29,45 @@ class ColorTest extends TestCase
         $index = $color->getIndex($im);
         $this->assertNotEquals(-1, $index);
     }
+
+    public function testToArray()
+    {
+        $color = new Color(12, 34, 56);
+        $this->assertEquals(array(12, 34, 56), $color->toArray());
+    }
+
+    public function testFromHsl()
+    {
+        $table = [
+            [[0.5, 0.8, 0.3], [15, 138, 138]],
+            [[0.999, 1, 1], [255, 255, 255]],
+            [[0, 0, 0], [0, 0, 0]],
+            [[338/360, 0.85, 0.25], [118, 10, 49]],
+        ];
+
+        foreach ($table as $pair) {
+            list($hsl, $rgb) = $pair;
+            $color = Color::fromHsl($hsl[0], $hsl[1], $hsl[2]);
+
+            $this->assertEquals($rgb, $color->toArray());
+        }
+    }
+
+    public function testParseString()
+    {
+        $table = [
+            ['#000', [0, 0, 0]],
+            ['#fff', [255, 255, 255]],
+            ['#abcdef', [171, 205, 239]],
+            ['#FEDCBA', [254, 220, 186]],
+            ['FEDCBA', [254, 220, 186]],
+            ['#abc', [170, 187, 204]],
+            ['abc', [170, 187, 204]],
+        ];
+
+        foreach ($table as $pair) {
+            $color = Color::parseString($pair[0]);
+            $this->assertEquals($pair[1], $color->toArray());
+        }
+    }
 }
